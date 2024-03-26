2d6e88b518fbafec3c55d27469a762fa8ea6b47f
Anton767 <118614536+PandaBTBs@users.noreply.github.com>
2024-03-26 19:40:48 +0300

    Update tts.py

--- assistant/tts.py
+++ assistant/tts.py
@@ -27,7 +27,7 @@ def va_speak(what: str):
                             put_yo=put_yo)
 
     sd.play(audio, sample_rate * 1.05)
-    time.sleep((len(audio) / sample_rate) + 0.5)
+    time.sleep((len(audio) / sample_rate))
     sd.stop()
 
 # sd.play(audio, sample_rate)
